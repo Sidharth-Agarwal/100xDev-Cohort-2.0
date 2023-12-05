75c2564604ad9caf6a58bb3de9f140ddee7eb1bf
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-05 18:24:16 +0530

    Hellew

---
+++ Week-1/2Greet.js
@@ -0,0 +1,4 @@
+let firstName = "Sidharth";
+let lastName = "Agarwal";
+
+console.log("Hello, " + firstName + " " + lastName + "! Hope you are doing well.");
---
+++ Week-1/3GreetIf.js
@@ -0,0 +1,8 @@
+let gender = true;
+
+if (gender === true) {
+    console.log("Hello sir, hope yo are having a wonderful day!");
+}
+else {
+    console.log("Hello madam, hope yo are having a wonderful day!");
+}
