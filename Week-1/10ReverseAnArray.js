0a54f5b860d5b239db3b16d3f423e018b3dda173
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-08 01:59:29 +0530

    Reverse

---
+++ Week-1/10ReverseAnArray.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5, 6];
+
+let mid = (arr.length) / 2;
+
+for (let i = 0; i <= mid; i++){
+
+    [arr[(arr.length) - i - 1], arr[i]] = [arr[i], arr[(arr.length) - i - 1]];
+    // let temp = arr[i];
+    // arr[i] = arr[(arr.length) - i - 1];
+    // arr[(arr.length) - i - 1] = temp;
+}
+
+console.log(arr);
