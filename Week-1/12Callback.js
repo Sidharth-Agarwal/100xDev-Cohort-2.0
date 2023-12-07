f2b69bbab65b5c41771fa09fbfaa743e27e2e523
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-08 02:37:06 +0530

    Function and Callbacks

---
+++ Week-1/12Callback.js
@@ -0,0 +1,14 @@
+function sum(a, b, fnToCall) {
+    let result = a + b;
+    fnToCall(result);
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+const ans = sum(2, 3, displayResult);
