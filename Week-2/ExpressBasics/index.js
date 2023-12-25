1436a74494cb375c9c52a91fe775c020c14b49ce
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-26 03:47:28 +0530

    Express

--- Week-2/ExpressBasics/index.js
+++ Week-2/ExpressBasics/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
 // Route Handler
-app.get('/', function(req, res){
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.send('<b> Hi there!!! </b>')
+})
+
+app.post('/conversation', function (req, res) {
+  // console.log(req.headers["authorization"])
+  console.log(req.body)
+  res.send({
+    msg:"2+2=rrr4"
+  })
 })
 
 app.listen(port, function(){
