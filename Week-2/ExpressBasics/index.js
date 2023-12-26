c36523b7305ba2166bcca40e6cfcda6a15e30919
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-26 12:48:19 +0530

    Express

--- Week-2/ExpressBasics/index.js
+++ Week-2/ExpressBasics/index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.json());
+// app.use(express.json()); -> Similar
 
 // Route Handler
 app.get('/', function (req, res) {
@@ -15,6 +16,14 @@ app.post('/conversation', function (req, res) {
   console.log(req.body)
   res.send({
     msg:"2+2=rrr4"
+  })
+})
+
+app.post('/conversation-limit', function (req, res) {
+  const message = req.body.message;
+  console.log(message);
+  res.json({
+    output:"2+2=4"
   })
 })
 
