dbf377c8a2d7ebf9fbfb2b6bbd0322cc51e0702f
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-27 15:44:16 +0530

    More Express

---
+++ Week-2/Express More/Doctor1.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    const ans = calculateSum(a, b);
+    res.send(ans);
+})
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`)
+})
