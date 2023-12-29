b34e1a4475cb3910b737bf6cd343549d9ad62ca6
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-29 12:05:52 +0530

    Kidneys

--- Week-2/Express_More/KidneyAssignment.js
+++ Week-2/Express_More/KidneyAssignment.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const port = 3000;
+app.use(express.json());
 
 const users = [{
     name: 'John',
@@ -19,7 +20,7 @@ const users = [{
 app.get('/', function (req, res) {
     const johnKidneys = users[0].kidneys;
     const numberOfKidneys = johnKidneys.length;
-    const numberOfHealthyKidneys = 0;
+    let numberOfHealthyKidneys = 0;
     for (let i = 0; i < johnKidneys.length; i++){
         if (johnKidneys[i].healthy) {
             numberOfHealthyKidneys += 1;
@@ -34,13 +35,53 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
+    console.log(req.body);
     const isHealthy = req.body.isHealthy
     users[0].kidneys.push({
         healthy: isHealthy
     })
-    res.json({msg:'Done'})
+    res.json({
+        msg: 'Posted'
+    })
 })
 
+app.put('/', function (req, res) {
+    for (let i = 0; i < users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({
+        msg: 'Done'
+    })
+})
 
+app.delete('/', function (req, res) {
+    if (isOneUnhealthyPresent()) {
+        const newKidneys = []
+        for (let i = 0; i < users[0].kidneys.length; i++){
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({
+            msg: 'Deleted'
+        })
+    }
+    else {
+        res.status(411).json({msg:'No bad kidneys'})
+    }
+})
+
+function isOneUnhealthyPresent() {
+    let atLeastOneUnhealthyKidney = false;
+    for (let i = 0; i < users[0].kidneys.length; i++){
+        if (!users[0].kidneys[i].healthy) {
+            atLeastOneUnhealthyKidney = true;
+        }
+    }
+    return atLeastOneUnhealthyKidney
+}
 
 app.listen(port)
