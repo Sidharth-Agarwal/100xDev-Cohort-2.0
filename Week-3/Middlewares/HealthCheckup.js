12df3a8faee62fc123cf47ced300d187599948bf
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-31 16:39:31 +0530

    Healthy

--- Week-3/Middlewares/HealthCheckup.js
+++ Week-3/Middlewares/HealthCheckup.js
@@ -2,19 +2,55 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
-app.get('/health', (req, res) => {
+function userValidator(username, password) {
+    if (username != "sidharth" || password != "abcde") {
+        return false;
+    }
+    return true;
+}
+
+function kidneyValidator(username, password) {
+    if (kidneyId != 1 && kidneyId != 2) {
+        return false;
+    }
+    return true;
+}
+
+app.get('/health-checkup', (req, res) => {
     const kidneyId = req.query.kidneyId;
     const username = req.headers.username;
     const password = req.headers.password;
 
-    if (username != "sidharth" || password != "abcde") {
+    if (!userValidator(req.headers.username, req.headers.password)) {
         res.status(403).json({
             msg: 'User does not exist'
         })
         return;
     }
-    
-    if (kidneyId != 1 && kidneyId != 2) {
+
+    if (!kidneyValidator(req.query.kidneyId)) {
+        res.status(411).json({
+            msg: 'Wrong inputs'
+        })
+        return;
+    }
+
+    res.send('Your heart is healthy')
+})
+
+app.post('/replace-kidney', (req, res) => {
+    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    if (!userValidator(req.headers.username, req.headers.password)) {
+        res.status(403).json({
+            msg: 'User does not exist'
+        })
+        return;
+    }
+
+    if (!kidneyValidator(req.query.kidneyId)) {
         res.status(411).json({
             msg: 'Wrong inputs'
         })
