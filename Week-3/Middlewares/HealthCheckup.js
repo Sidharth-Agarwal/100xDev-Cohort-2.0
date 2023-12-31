e936877c43093020e08040dc838f10fac628f1eb
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-12-31 15:00:03 +0530

    Healthy

--- Week-3/Middlewares/HealthCheckup.js
+++ Week-3/Middlewares/HealthCheckup.js
@@ -2,26 +2,26 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
-app.get('/health-checkup', (req, res) => {
+app.get('/health', (req, res) => {
     const kidneyId = req.query.kidneyId;
-    const username = req.header.username;
-    const password = req.header.password;
+    const username = req.headers.username;
+    const password = req.headers.password;
 
-    if (username != 'Sidharth' || password != '1234') {
+    if (username != "sidharth" || password != "abcde") {
         res.status(403).json({
             msg: 'User does not exist'
         })
         return;
     }
-
-    if (kidneyId != 1 || kidneyId != 2) {
+    
+    if (kidneyId != 1 && kidneyId != 2) {
         res.status(411).json({
             msg: 'Wrong inputs'
         })
         return;
     }
 
-    res.send('Your heeart is healthy')
+    res.send('Your heart is healthy')
 })
 
 app.listen(port)
