49c63b9a4a611aeecd2c1cc4aa98d7690e0dffa6
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-01 01:04:27 +0530

    validate login

--- Week-3/Zod/ValidateLogin.js
+++ Week-3/Zod/ValidateLogin.js
@@ -2,3 +2,18 @@ const express = require("express")
 const app = express()
 const zod = require("zod")
 
+const schema = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8)
+})
+
+app.get('/login', function (req, res) {
+    const response = schema.safeParse(req.body);
+    
+    if (!response.success) {
+        res.json({
+            msg:"Your inputs are invalid"
+        })
+        return
+    }
+})
