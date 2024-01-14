c4ce206145a1f5714eaca76db49d61a40badea3f
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-15 03:51:02 +0530

    Fetching from backend

--- Week-4/Backend_FETCH/BasicBackend.js
+++ Week-4/Backend_FETCH/BasicBackend.js
@@ -4,10 +4,23 @@ const app = express();
 app.use(cors());
 
 app.get("/sum", (req, res) => {
-    const a = parseInt(req.query.a);
-    const b = parseInt(req.query.b);
-    const sum = a + b;
-    res.send(sum.toString());
-})
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
 
-app.listen(3000);
+app.get("/interest", (req, res) => {
+  const principal = parseInt(req.query.principal);
+  const rate = parseInt(req.query.rate);
+  const time = parseInt(req.query.time);
+  const interest = (principal * rate * time) / 100;
+  const total = principal + interest;
+  res.send({
+    total: total,
+    interest: interest,
+  })
+
+});
+
+app.listen(3000)
