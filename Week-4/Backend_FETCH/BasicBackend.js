30edd045f818252d0d417cffaa3e9bceb84f9b6b
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-18 03:22:46 +0530

    Simple Interest Complete

--- Week-4/Backend_FETCH/BasicBackend.js
+++ Week-4/Backend_FETCH/BasicBackend.js
@@ -18,11 +18,11 @@ app.get("/interest", (req, res) => {
   const time = parseInt(req.query.time);
   const interest = (principal * rate * time) / 100;
   const total = principal + interest;
+
   res.send({
     total: total,
     interest: interest,
   })
-
 });
 
 app.listen(3000)
