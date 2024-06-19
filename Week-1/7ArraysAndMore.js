f0664856e93fa47666cf4b9b3e3097a366a15304
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-06-20 00:07:33 +0530

    Update

--- Week-1/7ArraysAndMore.js
+++ Week-1/7ArraysAndMore.js
@@ -1,9 +1,2 @@
 const personArray = ["Sidharth", "Raman", "Priya"];
 const genderArray = ["Male", "Male", "Female"];
-numberOfUsers = personArray.length;
-
-for (let i = 0; i < numberOfUsers; i++){
-    if (genderArray[i] == "Male") {
-        console.log(personArray[i]);
-    }
-}
