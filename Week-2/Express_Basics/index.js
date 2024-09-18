690c5e773b76a2605525e8f77e30952b0582f113
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-09-18 14:23:15 +0530

    Another trial update

--- Week-2/Express_Basics/index.js
+++ Week-2/Express_Basics/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Route Handler
 app.get('/', function (req, res) {
-  res.status(401).send('<b> I Sidharth heree </b>')
+  res.status(401).send('<b> Hi </b>')
 })
 
 app.post('/conversation', function (req, res) {
