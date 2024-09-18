45f16a893dddbd42e3aae4070d1d9fa8f2840af8
sidharth-agarwal <sidharthagarwal.2003@gmail.com>
2024-09-18 14:22:57 +0530

    Trail update on express

--- Week-2/Express_Basics/index.js
+++ Week-2/Express_Basics/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Route Handler
 app.get('/', function (req, res) {
-  res.status(401).send('<b> Hi there!!! </b>')
+  res.status(401).send('<b> I Sidharth heree </b>')
 })
 
 app.post('/conversation', function (req, res) {
