849d4774031eedf6df0dfc23b30c08f2ce7b2cc8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-09-23 00:09:13 +0530

    Minor update

--- Week-2/Express_More/Doctor1.js
+++ Week-2/Express_More/Doctor1.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 3000
 
 function calculateSum(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 app.get('/', function (req, res) {
     const a = req.query.a;
     const b = req.query.b;
     const ans = calculateSum(a, b);
-    res.send(toString(ans));
+    res.send(ans.toString());
 })
 
 app.listen(port, function () {
